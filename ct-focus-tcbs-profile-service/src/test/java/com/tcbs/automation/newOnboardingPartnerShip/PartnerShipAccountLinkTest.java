66acc0877d081f731edbef2854ca02c252a36c17
phuongtt37 <phuongtt37@tcbs.com.vn>
2022-08-02 17:57:14 +0700

    writing testcase FOC-2078

---
+++ ct-focus-tcbs-profile-service/src/test/java/com/tcbs/automation/newOnboardingPartnerShip/PartnerShipAccountLinkTest.java
@@ -0,0 +1,96 @@
+package com.tcbs.automation.newOnboardingPartnerShip;
+
+import com.adaptavist.tm4j.junit.annotation.TestCase;
+import com.tcbs.automation.cas.TcbsPartnerShip;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import lombok.Getter;
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
+import net.thucydides.core.annotations.Title;
+import net.thucydides.junit.annotations.UseTestDataFrom;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.*;
+
+import static com.tcbs.automation.config.tcbsprofileservice.TcbsProfileServiceConfig.*;
+import static com.tcbs.automation.tools.FormatUtils.syncData;
+import static net.serenitybdd.rest.SerenityRest.given;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SerenityParameterizedRunner.class)
+@UseTestDataFrom(value = "data/newOnboardingPartnerShip/ApiPartnerShipAccountLink.csv", separator = '|')
+public class PartnerShipAccountLinkTest {
+  @Getter
+  private String testCaseName;
+  private int statusCode;
+
+  private String errorMessage;
+  private String partnerId;
+  private String partnerAccountId;
+  private String code105C;
+  private String idNumber;
+  private String birthday;
+  private String linkType;
+  private String accountNo;
+  private String accountName;
+  private HashMap<String, Object> body;
+
+
+  @Before
+  public void setup() {
+
+    linkType = syncData(linkType);
+    List<String> listLinkType = new ArrayList<>(Arrays.asList(linkType.split(",")));
+
+    HashMap<String, Object> iaBankAccount = new HashMap<>();
+    iaBankAccount.put("accountNo", accountNo);
+    iaBankAccount.put("accountName", accountName);
+
+    partnerId = syncData(partnerId);
+    partnerAccountId = syncData(partnerAccountId);
+    code105C = syncData(code105C);
+    idNumber = syncData(idNumber);
+    birthday = syncData(birthday);
+    accountNo = syncData(accountNo);
+    accountName = syncData(accountName);
+    body = new HashMap<>();
+
+    body.put("partnerId", partnerId);
+    body.put("partnerAccountId", partnerAccountId);
+    body.put("code105C", code105C);
+    body.put("idNumber", idNumber);
+    body.put("birthday", birthday);
+    body.put("linkType", listLinkType);
+
+    if (!testCaseName.contains("case linkType is ACCOUNT")) {
+      body.put("iaBankAccount", iaBankAccount);
+    }
+  }
+
+  @Test
+  @TestCase(name = "#testCaseName")
+  @Title("Verify partnership account link")
+  public void partnerShipAccountLink() {
+    System.out.println("Test Case: " + testCaseName);
+    RequestSpecification requestSpecification = given()
+      .baseUri(PARTNERSHIP_ACCOUNT_LINK)
+      .header("x-api-key", testCaseName.contains("invalid x-api-key") ? FMB_X_API_KEY : PARTNERSHIP_X_API_KEY);
+
+    Response response;
+
+    if (testCaseName.contains("missing body")) {
+      response = requestSpecification.post();
+    } else {
+      response = requestSpecification.body(body).post();
+    }
+
+    assertEquals(statusCode, response.getStatusCode());
+    if (statusCode == 200) {
+      assertEquals(partnerAccountId, TcbsPartnerShip.getPartnerShip(partnerAccountId).getPartnerAccountId());
+    } else {
+      assertEquals(errorMessage, response.jsonPath().get("message"));
+    }
+  }
+}
---
+++ tcbs-automation-db/src/main/java/com/tcbs/automation/cas/TcbsPartnerShip.java
@@ -0,0 +1,55 @@
+package com.tcbs.automation.cas;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.thucydides.core.annotations.Step;
+import org.hibernate.query.Query;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "TCBS_PARTNERSHIP")
+@Getter
+@Setter
+public class TcbsPartnerShip {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @NotNull
+  @Column(name = "ID")
+  private BigDecimal id;
+  @Column(name = "USER_ID")
+  private BigDecimal userId;
+  @Column(name = "PARTNER_ID")
+  private String partnerId;
+  @Column(name = "PARTNER_ACCOUNT_ID")
+  private String partnerAccountId;
+  @Column(name = "LINK_ACCOUNT_STATUS")
+  private String linkAccountStatus;
+  @Column(name = "LINK_ACCOUNT_TNC")
+  private String linkAccountTnc;
+  @Column(name = "LINK_ACCOUNT_DATE")
+  private Timestamp linkAccountDate;
+  @Column(name = "LINK_IA_STATUS")
+  private String linkIaStatus;
+  @Column(name = "LINK_IA_TNC")
+  private String linkIaTnc;
+  @Column(name = "LINK_IA_DATE")
+  private Timestamp linkIaDate;
+  @Column(name = "UPDATED_DATE")
+  private Timestamp updatedDate;
+  @Column(name = "CONFIRM_ID")
+  private String confirmId;
+
+  @Step
+  public static TcbsPartnerShip getPartnerShip(String partnerAccountId) {
+    Query<TcbsPartnerShip> query = CAS.casConnection.getSession().createQuery(
+      "from TcbsPartnerShip a where a.partnerAccountId=:partnerAccountId", TcbsPartnerShip.class);
+    query.setParameter("partnerAccountId", partnerAccountId);
+    return query.getSingleResult();
+  }
+}
